856e936efc37e965bc87ae18ecf5dbd642be0c5e
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2023-12-31 14:04:59 +0700

    feat: display preview pasted img

--- app.vue
+++ app.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 onMounted(() => {
-  // document.addEventListener('keydown', (event) => {
-  //   if (event.ctrlKey && event.key === 'v') {
-  //     console.log('Ctrl + V');
-  //   }
-  // });
-  const preview = document.querySelector("#preview");
-  document.addEventListener('paste', (event) => {
+  const preview = document.querySelector("#replace-with-preview");
+  document.addEventListener("paste", (event) => {
     event.preventDefault();
     event.stopPropagation();
 
@@ -14,31 +9,31 @@ onMounted(() => {
     console.log(items?.length);
     if (items) {
       for (const item of items) {
-        if (item.type.indexOf('image') !== -1) {
+        if (item.type.indexOf("image") !== -1) {
           const blob = item.getAsFile();
-          console.log('b', blob);
+          console.log("b", blob);
           const reader = new FileReader();
           reader.onload = (event) => {
             const img = new Image();
             img.src = event.target?.result as string;
-            console.log('img', img);
-            preview?.appendChild(img);
+            console.log("img", img);
+            preview?.replaceWith(img);
           };
           reader.readAsDataURL(blob!);
         }
       }
     }
-  })
+  });
 });
-;
 </script>
 
 <template>
   <main class="">
     <section class="h-screen">
-      <div class="h-3/4 m-4 border border-gray-600 rounded-3xl text-center">
-        <h1 class="text-lg font-bold text-white">Press Ctrl + V to download</h1>
-        <div id="preview" />
+      <div class="h-3/4 m-4 grid place-items-center border border-gray-600 rounded-3xl">
+        <h1 id="replace-with-preview" class="text-lg font-bold text-white">
+          Paste to begin
+        </h1>
       </div>
     </section>
   </main>
