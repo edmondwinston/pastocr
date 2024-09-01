50d5980e6b2cfd6d195a937e0cb54ca5e714caef
edmondwinston <132081506+edmondwinston@users.noreply.github.com>
2024-09-01 10:08:39 +0700

    feat: pages

---
+++ pages/ocr.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+const shouldShowOCRButton = ref(false);
+
+interface RequestData {
+  imageBase64: string;
+  languageIndex: string;
+}
+function postOcr(data: RequestData) {
+  return $fetch('https://app.easyscreenocr.com/api/ocr/GetBaiduOcrTextNew', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+let imgBase64: string;
+onMounted(() => {
+  const preview = document.querySelector("#replace-with-preview");
+  document.addEventListener("paste", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const items = event.clipboardData?.items;
+    if (items) {
+      for (const item of items) {
+        console.log(item.type);
+        if (item.type.indexOf("image") !== -1) {
+          const blob = item.getAsFile();
+          const reader = new FileReader();
+          reader.onload = async (event) => {
+            const img = new Image();
+            imgBase64 = event.target?.result as string;
+
+            img.src = imgBase64;
+            preview?.replaceWith(img);
+
+            shouldShowOCRButton.value = true;
+          };
+          reader.readAsDataURL(blob!);
+        }
+      }
+    }
+  });
+});
+
+async function OCRButtonOnClickHandler() {
+  try {
+    const resq = await postOcr({ imageBase64: imgBase64, languageIndex: '' });
+    console.log(resq);
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
+
+<template>
+  <main class="">
+    <section class="h-screen">
+      <div class="h-3/4 m-4 grid place-items-center border border-gray-600 rounded-3xl">
+        <h1 id="replace-with-preview" class="text-lg font-bold text-white">
+          Paste to begin
+        </h1>
+
+        <UButton icon="i-heroicons-document-text" size="sm" color="primary" variant="soft" :trailing="false"
+          v-if="shouldShowOCRButton" @click="OCRButtonOnClickHandler">
+          OCR this
+        </UButton>
+      </div>
+    </section>
+  </main>
+</template>
